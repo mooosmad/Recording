7071db23f6bbb651883b647c71bd54da905e0f88
mooosmad <mooosmad@gmail.com>
2022-07-06 21:21:23 +0000

    Initial Commit

---
+++ AnimationTutorial/AnimationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTutorialApp.swift
+//  AnimationTutorial
+//
+//  Created by Mo Smad on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
